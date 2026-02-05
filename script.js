ab26238c044b61aa04668dfc30956e1259332442
Mohammad khan <khankadir2703@gmail.com>
2026-02-05 19:00:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+let coins = 0;
 
+function watchAd() {
+  alert("Ad dekh rahe ho... 10 sec wait karo");
+
+  setTimeout(() => {
+    coins += 5;
+    document.getElementById("coins").innerText = coins;
+    alert("🎉 You earned 5 coins!");
+  }, 10000);
+}
